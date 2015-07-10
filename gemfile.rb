562625c245db3d521c58386b1815e509915ae4de
MarkHyland <markdhyland@gmail.com>
2015-07-10 15:28:29 -0400

    Create gemfile.rb

---
+++ gemfile.rb
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+ 
+gem 'rails', '4.2.2'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'devise'
+gem 'twitter-bootstrap-rails'
+gem 'bootstrap_form', github: 'bootstrap-ruby/rails-bootstrap-forms'
+ 
+group :development, :test do
+  gem 'pry-rails'
+  gem 'spring'
+  gem 'quiet_assets'
+  gem 'faker'
+end
+ 
+group :development do
+  gem 'better_errors'
+  gem 'did_you_mean'
+  gem 'binding_of_caller'
+  gem 'sqlite3'
+end
+ 
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+end
