7a17fc7e41c8e060caf874e42da4d82f25bc34ac
MarkHyland <markdhyland@gmail.com>
2015-07-10 17:18:14 -0400

    Update existing.rb
    Adding Source Path?

--- existing.rb
+++ existing.rb
@@ -1,40 +1,40 @@
-#%w(
-#  quiet_assets
-#  pry-rails
-#).each { |g| gem g }
-gem 'quiet_assets'
-gem 'pry-rails'
-
-gem_group :development do
-  gem 'better_errors'
-  gem 'did_you_mean'
-  gem 'binding_of_caller'
+def source_paths
+  [File.expand_path(File.dirname(__FILE__))]
 end
-
-# Move sqlite3 to development
-comment_lines "Gemfile", /sqlite/
-gem_group :development do
-  gem 'sqlite3'
-end
-gem_group :production do
-  gem 'pg'
-  gem 'rails_12factor'
-end
-
-comment_lines "Gemfile", /byebug/
-comment_lines "Gemfile", /web-console/
-
-require 'pry'
-binding.pry
-
-if yes?("Use devise?")
-  gem 'devise'
-  generate 'devise:install'
-  generate 'devise', 'User'
-end
-
-after_bundle do
-  git :init
-  git add: "."
-  git commit: "-m 'Initial commit'"
-end
+  
+  gem 'quiet_assets'
+  gem 'pry-rails'
+  
+  gem_group :development do
+    gem 'better_errors'
+    gem 'did_you_mean'
+    gem 'binding_of_caller'
+  end
+  
+  # Move sqlite3 to development
+  comment_lines "Gemfile", /sqlite/
+  gem_group :development do
+    gem 'sqlite3'
+  end
+  gem_group :production do
+    gem 'pg'
+    gem 'rails_12factor'
+  end
+  
+  comment_lines "Gemfile", /byebug/
+  comment_lines "Gemfile", /web-console/
+  
+  require 'pry'
+  binding.pry
+  
+  if yes?("Use devise?")
+    gem 'devise'
+    generate 'devise:install'
+    generate 'devise', 'User'
+  end
+  
+  after_bundle do
+    git :init
+    git add: "."
+    git commit: "-m 'Initial commit'"
+  end
